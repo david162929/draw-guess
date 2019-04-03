7d3c576b6de382cb6972f5148de70ca12b713b14
David Chen <45975185+david162929@users.noreply.github.com>
2019-04-03 15:36:55 +0800

    add link database

--- app2.js
+++ app2.js
@@ -1,7 +1,56 @@
 /* ---------------Module--------------- */
 const express = require("express");
+const mysql = require('mysql2');
+const Client = require('ssh2').Client;
 
 const app = express();
+
+
+//create TCP connection to MySQL over SSH by using mysql2 and ssh2 module
+let sql;	//can't use const
+
+const ssh = new Client();
+ssh.on('ready', function() {
+	ssh.forwardOut(
+		'127.0.0.1',
+		12345,
+		'127.0.0.1',
+		3306,
+		function (err, stream) {
+			if (err) throw err;
+			// Create the connection pool. The pool-specific settings are the defaults
+/* 			pool = mysql.createPool({
+			  user: 'root',
+			  database: 'stylish',
+			  password: 'daviddata1357',
+			  stream: stream,
+			  waitForConnections: true,
+			  connectionLimit: 100,
+			  queueLimit: 0
+			});	 */	
+			
+			sql = mysql.createConnection({
+				user: 'root',
+				database: 'stylish',
+				password: 'daviddata1357',
+				stream: stream // <--- this is the important part
+			});
+			
+			// use sql connection as usual
+			sql.query("SELECT id FROM product", function (err, result, fields) {
+				if (err) throw err;
+				console.log("Connect to MySQL succeed!");
+			});
+	
+		});
+	}).connect({
+	// ssh connection config ...
+	host: '52.15.89.192',
+	port: 22,
+	username: 'ec2-user',
+	privateKey: require('fs').readFileSync(".ssh/2019-4-3-keyPair.pem")
+}); 
+
 
 
 app.get("/", (req, res) => {
