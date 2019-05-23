9293c38323e6999c571d5ee98d5e47649f5bfe37
David Chen <45975185+david162929@users.noreply.github.com>
2019-05-23 09:13:36 +0800

    add date

--- socket_io/draw.js
+++ socket_io/draw.js
@@ -22,6 +22,7 @@ function drawSocket(io, pool) {
     const draw = io.of("/draw");
     draw.on("connection", (socket) => {
         console.log("2019.5.22");
+        console.log(Date().toString());
         console.log("draw connected.");
 
         // change user id
